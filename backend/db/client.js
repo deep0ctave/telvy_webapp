33d088482e6dc27f0f2f8e39cb1676d66e4ec6d8
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-08-02 23:34:03 +0530

    Fix remote db issue

--- backend/db/client.js
+++ backend/db/client.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  host: "quiz-databse-test-server.postgres.database.azure.com",
+  user: "demouser",
+  password: "avinash@400",
+  database: "postgres",
+  port: 5432,
   ssl: true,
 });
 
