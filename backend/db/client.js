2f93954f01db46c53d258cabd945dbd5b92cef02
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-08-02 22:15:43 +0530

    Add db password

--- backend/db/client.js
+++ backend/db/client.js
@@ -2,6 +2,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({host:"quiz-databse-test-server.postgres.database.azure.com", user:"demouser", password:"{your_password}", database:"postgres", port:5432, ssl:true});
+const pool = new Pool({host:"quiz-databse-test-server.postgres.database.azure.com", user:"demouser", password:"avinash@400", database:"postgres", port:5432, ssl:true});
 
 module.exports = pool;
