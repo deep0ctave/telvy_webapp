821b83a4cefdb749cce548438f481250f62ce7f0
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-08-02 22:09:28 +0530

    Fix ssl

--- backend/db/client.js
+++ backend/db/client.js
@@ -2,6 +2,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({host:"quiz-databse-test-server.postgres.database.azure.com", user:"demouser", password:"avinash@400", database:"postgres", port:5432, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
+const pool = new Pool({host:"quiz-databse-test-server.postgres.database.azure.com", user:"demouser", password:"{your_password}", database:"postgres", port:5432, ssl:true});
 
 module.exports = pool;
